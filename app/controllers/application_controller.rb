460a3fd658c0de2bddc3fa2983647cac013ba156
s20211435 <yotamoti415717@gmail.com>
2025-10-13 12:25:02 +0900

    ログイン画面の修正とヘッダーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  layout :layout_by_resource
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -17,5 +19,15 @@ class ApplicationController < ActionController::Base
     # ログイン後にセッションフラグを設定
     session[:show_reminders_today] = true
     stored_location_for(resource) || root_path
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
   end
 end
